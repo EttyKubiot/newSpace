f55f514b97a9239d3ad037a77f3c1398d32b550a
EttyBlack <6166887@gmail.com>
2022-01-12 10:54:36 +0200

    Try

--- Assets/etty/DisplayImageFromDrive.cs
+++ Assets/etty/DisplayImageFromDrive.cs
@@ -3,42 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class DisplayImageFromDrive : MonoBehaviour
 {
     [SerializeField] private string[] urlI;
     //private string DriveImageURL = "https://drive.google.com/u/0/uc?id=11YxceNYNHKKEidjF1El40msaoyNi84cg&export=download";
     //private string DriveImageURL2 = "https://drive.google.com/u/0/uc?id=1wdndhNdWoGEu_GK997NCbeAm5viJRI_5&export=download";
     private int imagePlace;
-    [SerializeField] private GameObject [] stand1;
-    [SerializeField] private GameObject[] stand2;
-    void Start()
-    {
-        //imagePlace = 0;
-       
-        StartCoroutine(DownLoadImage());
-      
-    }
+    //[SerializeField] private GameObject[] stand1;
+    //[SerializeField] private GameObject[] stand2;
+    //Dictionary<GameObject, GameObject> keys = new Dictionary<GameObject, GameObject>();
+    List<GameObject[]> stands = new List<GameObject[]>();
+    //List<GameObject> set = new List<GameObject>();
+    [SerializeField] private GameObject[] set;
 
-    private IEnumerator DownLoadImage()
-    {
-     
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(urlI[0]);
-        yield return request.Send();
-        //imagePlace++;
-      
-        for (int i = 0; i < stand1.Length; i++)
-        {
-          
-          stand1[i].GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(request);
-          
-        }
-        UnityWebRequest request2 = UnityWebRequestTexture.GetTexture(urlI[1]);
-        yield return request2.Send();
+    //void Start()
+    //{
+    //imagePlace = 0;
+    //    for (int i = 0; i < keys.Count; i++)
+    //    {
+    //        keys.Add(stand1[i], stand2[i]);
+    //    }
+    //        StartCoroutine(DownLoadImage());
 
-        for (int i = 0; i < stand2.Length; i++)
-        {
-          stand2[i].GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(request2);
-        }
+    //}
 
-    }
+    //private IEnumerator DownLoadImage()
+    //{
+    // for(int i = 0; i < urlI.Length; i++)
+    //    {
+    //        UnityWebRequest request = UnityWebRequestTexture.GetTexture(urlI[i]);
+    //        yield return request.Send();
+    //        keys[stand1[i]].GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(request);
+
+    //        //for (int b = 0; b < keys.Count; b++)
+    //        //{
+
+
+
+    //}
+
+    //}
+
+    //imagePlace++;
+
+
+    //UnityWebRequest request2 = UnityWebRequestTexture.GetTexture(urlI[1]);
+    //yield return request2.Send();
+
+    //for (int i = 0; i < stand2.Length; i++)
+    //{
+    //    stand2[i].GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(request2);
+    //}
+
+    //}
 }
